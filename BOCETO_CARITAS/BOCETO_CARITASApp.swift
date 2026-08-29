794b44d5da043e7a151b69a3b63782e96c063b4c
Rogelio González <rg1205007@gmail.com>
2026-08-29 10:29:04 -0600

    Initial Commit

---
+++ BOCETO_CARITAS/BOCETO_CARITASApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOCETO_CARITASApp.swift
+//  BOCETO_CARITAS
+//
+//  Created by Rogelio Iram González Ortiz on 29/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BOCETO_CARITASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BOCETO_CARITAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BOCETO_CARITAS
+//
+//  Created by Rogelio Iram González Ortiz on 29/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
