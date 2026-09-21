f879102f05dc516f3f66fb4bbfe31ae9cf348880
Jaime-2316 <jaimevalo06@gmail.com>
2026-09-21 09:27:51 -0600

    Service Login

---
+++ BOCETO_CARITAS/Model/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  BOCETO_CARITAS
+//
+//  Created by Alumno on 21/09/26.
+//
+
+import Foundation
+
+
+struct LoginRequest: Codable {
+    let userName: String
+    let password_hash: String
+}
+
+struct LoginResponse: Codable {
+    let nombre: String
+    let idRole: Int
+    let error: String?
+}
